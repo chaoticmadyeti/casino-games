d6157e2f7fe00dbb1c610405304e3e3a614cc039
chaoticmadyeti <yipingpanxf@gmail.com>
2026-05-18 21:39:10 +1000

    added double street bets

--- roulette.py
+++ roulette.py
@@ -4,22 +4,23 @@ import sys
 
 # Wheel and colours (European)
 
-wheel = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
-red_numbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36] 
-black_numbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
-half_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] 
-half_2 = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
-odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35] 
-even = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
-col_1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
-col_2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35] 
-col_3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
-dozen_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-dozen_2 = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24] 
-dozen_3 = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
-top_line = [0, 1, 2, 3]
+wheel = ['0', '32', '15', '19', '4', '21', '2', '25', '17', '34', '6', '27', '13', '36', '11', '30', '8', '23', '10', '5', '24', '16', '33', '1', '20', '14', '31', '9', '22', '18', '29', '7', '28', '12', '35', '3', '26']
+red_numbers = ['1', '3', '5', '7', '9', '12', '14', '16', '18', '19', '21', '23', '25', '27', '30', '32', '34', '36'] 
+black_numbers = ['2', '4', '6', '8', '10', '11', '13', '15', '17', '20', '22', '24', '26', '28', '29', '31', '33', '35']
+half_1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'] 
+half_2 = ['19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36']
+odd = ['1', '3', '5', '7', '9', '11', '13', '15', '17', '19', '21', '23', '25', '27', '29', '31', '33', '35'] 
+even = ['2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30', '32', '34', '36']
+col_1 = ['1', '4', '7', '10', '13', '16', '19', '22', '25', '28', '31', '34']
+col_2 = ['2', '5', '8', '11', '14', '17', '20', '23', '26', '29', '32', '35'] 
+col_3 = ['3', '6', '9', '12', '15', '18', '21', '24', '27', '30', '33', '36']
+dozen_1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+dozen_2 = ['13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'] 
+dozen_3 = ['25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36']
+streets = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['10', '11', '12'], ['13', '14', '15'], ['16', '17', '18'], ['19', '20', '21'], ['22', '23', '24'], ['25', '26', '27'], ['28', '29', '30'], ['31', '32', '33'], ['34', '35', '36']]
+top_line = ['0', '1', '2', '3']
 
-bets = ["colour", "parity", "half", "column", "dozen"]
+bets = ["colour", "parity", "half", "column", "dozen", "double street"]
 
 content = ''
 
@@ -40,9 +41,11 @@ def colour(number):
     GREEN = "\033[1;37;42m"
     RESET = "\033[0m"
 
-    if int(number) == 0:
+    new_num = number.replace(' ', '')
+
+    if new_num == '0':
         return f"{GREEN}{number}{RESET}"
-    elif int(number) in red_numbers:
+    elif new_num in red_numbers:
         return f"{RED}{number}{RESET}"
     else:
         return f"{BLACK}{number}{RESET}"
@@ -87,7 +90,7 @@ def bet_type():
     global multi
 
     print("What do you want to bet on? (Type 'exit' to quit)")
-    print("Currently, this code has: Colour, Parity, Half, Column, Dozen.")
+    print("Currently, this code has: Colour, Parity, Half, Column, Dozen, Double Street.")
     specific_bet = input("Choice: ")
 
     while specific_bet.lower() not in bets:
@@ -195,6 +198,19 @@ def bet_type():
             return dozen_2
         elif specific_bet == '3':
             return dozen_3
+        
+    # Double Street Bets
+
+    elif specific_bet.lower() == "double street":
+        multi = 5
+        print("Please type the lowest number in the double street you would like to bet on.")
+        specific_bet = input("Choice: ")
+        while specific_bet.lower() not in col_1:
+            print("That is not a valid number. Please type the lowest number in the double street you would like to bet on.")
+            specific_bet = input("Choice: ")
+        for i in range(len(streets) - 1):
+            if streets[i][0] == specific_bet.lower():
+                return streets[i] + streets[i + 1]
 
 
 # Main Logic

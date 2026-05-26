817ba801b8f2d688e649d769497498e6c169bb96
chaoticmadyeti <yipingpanxf@gmail.com>
2026-05-26 12:08:49 +1000

    added planning framework

--- texas hold em.py
+++ texas hold em.py
@@ -18,3 +18,63 @@ if content.isdigit():
 else:
     print("Please put a positive integer in 'money.txt'")
     sys.exit()
+
+# Check hand and community cards combination
+
+def royal_flush(cards):
+    pass
+
+def straight_flush(cards):
+    pass
+
+def four_of_a_kind(cards):
+    pass
+
+def full_house(cards):
+    pass
+
+def flush(cards):
+    pass
+
+def straight(cards):
+    pass
+
+def three_of_a_kind(cards):
+    pass
+
+def two_pair(cards):
+    pass
+
+def pair(cards):
+    pass
+
+# Find hand value
+
+def check_value(cards):
+    # How the hand values are stored:
+    # value = ['card value name', 'high card', 'secondary card (if not applicable, this will be 0)']
+    pass
+
+# Check who wins
+
+def showdown(all_player_hands): 
+    # all_player_hands is an array of all players at the end
+
+    # Call check_value() on every hand and determine who wins
+    pass
+
+# Handle money
+
+def bet_handling(pots): 
+    # Array of all pots, includes side pots
+    # Determine who gets how much money
+    pass
+
+# Core loop
+
+def texas_hold_em():
+    # Handles the dealing of cards, betting in each round etc. (aka all of the text stuff that actually shows up on screen)
+    # All above functions are no-text (no print() functions)
+    pass
+
+texas_hold_em()

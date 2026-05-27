5107847940c4072cd90bf506f9fa524c5bf28293
chaoticmadyeti <yipingpanxf@gmail.com>
2026-05-27 20:53:57 +1000

    added winner determining logic

--- texas hold em.py
+++ texas hold em.py
@@ -19,7 +19,9 @@ class player:
     def __init__(self, id, hole_cards, score):
         self.id = id # ID number 1 will be the human player.
         self.hole_cards = hole_cards
-        self.score = score
+        self.score = score 
+
+# Score will be 1 - 9 depending on the value of the hand, and all other elements in the list will be tiebreaker / kicker cards
 
 deck = [card(suit=s, rank=r) for s in suits for r in ranks]
 
@@ -47,7 +49,25 @@ def eval_hand(cards):
 # Rank all players from best to worst (we need to rank all players to deal with side pots down the line)
 
 def find_winner(community_cards, players):
-    pass
+    for player in players:
+        all_cards = community_cards + player.hole_cards
+        combinations = itertools.combinations(all_cards, 5)
+
+        best_score = [0]
+
+        for combination in combinations:
+            current_score = eval_hand(combination)
+            for i in range(len(current_score)):
+                if current_score[i] > best_score[i]:
+                    best_score = current_score
+                    break
+        
+        player.score = best_score
+
+    sorted_players = sorted(players, reverse=True)
+
+    return sorted_players
+
 
 # Core betting logic
 

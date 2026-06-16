41dcbe0dfece8c5e24fae358aacd7229a7ac70c9
chaoticmadyeti <yipingpanxf@gmail.com>
2026-06-16 21:17:12 +1000

    added bingo check

--- raw python scripts/bingo.py
+++ raw python scripts/bingo.py
@@ -29,41 +29,63 @@ numbers_g = range(46, 61)
 numbers_o = range(61, 76)
 
 def create_cards():
-    card = [[], [], [], [], []]
+    card = []
 
     curr_numbers_b = numbers_b.copy()
     for _ in range(5):
         number = random.randint(0, 14)
-        card[0].append(curr_numbers_b[number])
+        card.append(curr_numbers_b[number])
         curr_numbers_b.pop(card)
     
     curr_numbers_i = numbers_i.copy()
     for _ in range(5):
         number = random.randint(0, 14)
-        card[1].append(curr_numbers_i[number])
+        card.append(curr_numbers_i[number])
         curr_numbers_i.pop(card)
     
     curr_numbers_n = numbers_n.copy()
     for _ in range(5):
         number = random.randint(0, 14)
-        card[2].append(curr_numbers_n[number])
+        card.append(curr_numbers_n[number])
         curr_numbers_n.pop(card)
     
     curr_numbers_g = numbers_g.copy()
     for _ in range(5):
         number = random.randint(0, 14)
-        card[3].append(curr_numbers_g[number])
+        card.append(curr_numbers_g[number])
         curr_numbers_g.pop(card)
     
     curr_numbers_o = numbers_o.copy()
     for _ in range(5):
         number = random.randint(0, 14)
-        card[4].append(curr_numbers_o[number])
+        card.append(curr_numbers_o[number])
         curr_numbers_o.pop(card)
     
-    card[2][2] = "FREE"
+    card[12] = "FREE"
 
     return card
+
+def check_bingo(card_tokens): # card_tokens is an array of all numbers gotten
+    winning_lines = [
+        {0, 1, 2, 3, 4}, 
+        {5, 6, 7, 8, 9},
+        {10, 11, 12, 13, 14},
+        {15, 16, 17, 18, 19},
+        {20, 21, 22, 23, 24},
+        {0, 5, 10, 15, 20},
+        {1, 6, 11, 16, 21},
+        {2, 7, 12, 17, 22},
+        {3, 8, 13, 18, 23},
+        {4, 9, 14, 19, 24},
+        {0, 6, 12, 18, 24},
+        {4, 8, 12, 16, 20}
+    ]
+
+    for line in winning_lines:
+        if line.issubset(card_tokens):
+            return True
+    
+    return False
     
 def bingo():
     pass

c9a13801dd5a1f10f45206b648c8fb65d9d86d18
chaoticmadyeti <yipingpanxf@gmail.com>
2026-06-16 18:10:46 +1000

    created bingo file

---
+++ raw python scripts/bingo.py
@@ -0,0 +1,28 @@
+import random
+import os
+import sys
+import time
+
+if getattr(sys, 'frozen', False):
+    exe_dir = os.path.dirname(sys.executable)
+else:
+    exe_dir = os.path.dirname(os.path.abspath(__file__))
+
+os.chdir(exe_dir)
+
+content = ''
+money = 0
+
+with open("money.txt", "r") as f:
+    content = f.read()
+
+if content.isdigit():
+    money = int(content)
+else:
+    print("Please put a positive integer in 'money.txt'")
+    sys.exit()
+
+numbers = range(1, 76)
+
+def bingo():
+    pass
